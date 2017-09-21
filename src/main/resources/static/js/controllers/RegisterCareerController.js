58b06f3e871efcc2e282be25c3d0c6b755d09573
huasocristian <cristian.espinoza.s@usach.cl>
2017-09-21 16:57:20 -0300

    Finish view registerCareer

--- src/main/resources/static/js/controllers/RegisterCareerController.js
+++ src/main/resources/static/js/controllers/RegisterCareerController.js
@@ -1,13 +1,43 @@
-app.controller('RegisterCareerController', ['$scope','$location', function($scope,$location) {
+app.controller('RegisterCareerController', ['$scope','$location','$http', function($scope,$location,$http) {
+
+    $scope.careers = [];
+
+    $scope.resetNewCareer = function(){
+                $scope.newCareer = {
+                "name":"",
+                }
+    }
+
+    $scope.loadCareers = function()
+    {
+        $http.get('/careers').then(function(response)
+        {
+           $scope.careers = response.data._embedded.careers;
+        });
+    }
 
     $scope.send = function(){
                 console.log($scope.newCareer);
+
                 var career = {
                     "name":$scope.newCareer.name,
-                }
+                };
+
                 $http.post('/careers',career)
                 .then(function(response){
                     console.log(response);
+                    $scope.loadCareers();
+
+                $scope.loadCareers();
                 });
-        }
+    }
+
+    $scope.deletedCareers = function(){
+                console.log($scope.deletedCareers);
+                $http.delete($scope.deletedCareers).then(function(response){
+                    console.log(response);
+                });
+                $scope.loadCareers();
+    }
+
 }]);
