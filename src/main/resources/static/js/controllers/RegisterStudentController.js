9ced00074a29faebf4d5e1f76b0a3ccba4d6f26a
huasocristian <cristian.espinoza.s@usach.cl>
2017-09-21 12:03:04 -0300

    Create new students and create new carrer

--- src/main/resources/static/js/controllers/RegisterStudentController.js
+++ src/main/resources/static/js/controllers/RegisterStudentController.js
@@ -1,5 +1,37 @@
 app.controller('RegisterStudentController', ['$scope','$location', function($scope,$location) {
 
-    $scope.title = "Agregar nueva usuario";
+    $scope.students = [];
+
+    $scope.resetNewStudent = function(){
+            $scope.newStudent = {
+            "name":"",
+            "mail":"",
+            "ingress":0,
+            "rut":0,
+            }
+    }
+
+    $scope.send = function(){
+            console.log($scope.newStudent);
+            var student = {
+                "name":$scope.newStudent.name,
+                "mail":$scope.newStudent.mail,
+                "ingress":$scope.newStudent.ingress,
+                "rut":$scope.newStudent.rut,
+            };
+
+            $scope.loadStudents();
+            });
+    }
+
+    $scope.deletedStudents = function(){
+            console.log($scope.deletedStudents);
+            $http.delete($scope.deletedStudents).then(function(response){
+                console.log(response);
+            });
+            $scope.loadStudents();
+    }
+
+    $scope.loadStudents();
 
 }]);
