15a09be5ab859619d875e48f37c0fdce6dd35e73
Javier <javier.arredondo.c@usach.cl>
2017-09-21 04:41:00 -0300

    File of SQL data done

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+--careers
+insert into career (name) values ('Ingeniería Civil Informática');
+insert into career (name) values ('Ingeniería Civil Eléctrica');
+insert into career (name) values ('Ingeniería Ejecución Informática');
+insert into career (name) values ('Ingeniería Civil en Memes');
+
+--students
+insert into student (ingress, mail, name, rut, id_career) values (2015, 'javier.arredondo.c@usach.cl', 'Javier', 194487185, 1);
+insert into student (ingress, mail, name, rut, id_career) values (2015, 'cristian.espinoza.s@usach.cl', 'Cristian', 191818970, 1);
+insert into student (ingress, mail, name, rut, id_career) values (2011, 'goku.kakaroto@usach.cl', 'Goku', 123456789, 2);
+insert into student (ingress, mail, name, rut, id_career) values (2011, 'izuku.midoriya@usach.cl', 'Izuku', 987654321, 3);
+insert into student (ingress, mail, name, rut, id_career) values (2011, 'rick.sanchez@usach.cl', 'Rick', 543216789, 4);
